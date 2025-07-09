2a35f4168246d6b477d7cb65cef6095f688706eb
dev-debasis <debasiskhamari7@gmail.com>
2025-07-10 00:28:31 +0530

    feat: add tweet controller with create, fetch, delete and other logic

---
+++ server/src/controllers/tweet.controller.js
@@ -0,0 +1,103 @@
+import { Tweet } from "../models/tweet.model.js";
+
+const createTweet = async (req, res) => {
+  try {
+    const { content } = req.body;
+
+    if (!content?.trim()) {
+      return res.status(400).json({
+        message: "Tweet content is required.",
+      });
+    }
+
+    const tweet = await Tweet.create({
+      userId: req.user._id,
+      content
+    });
+
+    return res.status(201).json({
+      message: "Tweet created successfully",
+      tweet,
+    });
+  } catch (error) {
+    console.error("Error creating tweet:", error);
+    return res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+const getAllTweets = async (req, res) => {
+  try {
+    const tweets = await Tweet.find()
+      .populate("userId", "name, username avatar")
+      .sort({ createdAt: -1 });
+
+    return res.status(200).json({
+      message: "All tweets fetched",
+      tweets,
+    });
+  } catch (error) {
+    console.error("Error fetching tweets:", error);
+    return res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+const getUserTweets = async (req, res) => {
+  try {
+    const { userId } = req.params;
+
+    const tweets = await Tweet.find({ userId })
+      .populate("userId", "name, username avatar")
+      .sort({ createdAt: -1 });
+
+    return res.status(200).json({
+      message: "User tweets fetched",
+      tweets,
+    });
+  } catch (error) {
+    console.error("Error fetching user tweets:", error);
+    return res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+const deleteTweet = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const tweet = await Tweet.findById(id);
+    if (!tweet) {
+      return res.status(404).json({
+        message: "Tweet not found",
+      });
+    }
+
+    if (tweet.userId.toString() !== req.user._id.toString()) {
+      return res.status(403).json({
+        message: "You are not authorized to delete this tweet",
+      });
+    }
+
+    await Tweet.findByIdAndDelete(id);
+
+    return res.status(200).json({
+      message: "Tweet deleted successfully",
+    });
+  } catch (error) {
+    console.error("Error deleting tweet:", error);
+    return res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+export {
+    createTweet,
+    getAllTweets,
+    getUserTweets,
+    deleteTweet
+}
