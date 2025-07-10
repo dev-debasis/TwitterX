e065ef569f2a159cf6388fe3439d7304e3da78bc
dev-debasis <debasiskhamari7@gmail.com>
2025-07-10 18:28:26 +0530

    feat: add like controller with like and unlike tweet functionality

---
+++ server/src/controllers/like.controller.js
@@ -0,0 +1,36 @@
+import { Like } from "../models/like.model.js";
+import { Tweet } from "../models/tweet.model.js";
+
+const toggleLike = async (req, res) => {
+  try {
+    const { id: tweetId } = req.params;
+    const userId = req.user._id;
+
+    const existingLike = await Like.findOne({ userId, tweetId });
+
+    if (existingLike) {
+      await Like.deleteOne({ _id: existingLike._id });
+      await Tweet.findByIdAndUpdate(tweetId, { $inc: { likeCounts: -1 } });
+
+      return res.status(200).json({ 
+        message: "Tweet unliked successfully." 
+    });
+    } else {
+      await Like.create({ userId, tweetId });
+      await Tweet.findByIdAndUpdate(tweetId, { $inc: { likeCounts: 1 } });
+
+      return res.status(200).json({ 
+        message: "Tweet liked successfully." 
+    });
+    }
+  } catch (error) {
+    console.error("Error in toggleLike:", error);
+    return res.status(500).json({ 
+        message: error.message 
+    });
+  }
+};
+
+export {
+  toggleLike
+}
