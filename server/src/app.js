7d6004e21ff15a16147404d4c2e7f5cb90a81887
dev-debasis <debasiskhamari7@gmail.com>
2025-07-10 18:50:12 +0530

    chore: update base app routing and dependencies

--- server/src/app.js
+++ server/src/app.js
@@ -1,15 +1,30 @@
 import express, { urlencoded } from "express";
 import cors from "cors"
+import cookieParser from "cookie-parser"
+
+import userRouter from "./routes/user.route.js"
+import tweetRouter from "./routes/tweet.route.js"
+
 const app = express()
 
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true
 }))
-
+app.use(urlencoded({
+    extended: true
+}))
 app.use(express.json())
-app.use("/", (req, res)=> {
-    res.send("Check")
+app.use(cookieParser())
+app.use("/uploads", express.static("uploads"))
+
+
+app.use("/api/v1/users", userRouter)
+app.use("/api/v1/tweets", tweetRouter)
+app.use("*", (req, res) => {
+    res.status(404).json({
+        message: "Route not found"
+    })
 })
 
 export { app }
