5309fe7b5f1450448585b6bc8719dba374264c71
dev-debasis <debasiskhamari7@gmail.com>
2025-07-19 19:31:06 +0530

    chore: basic structures and project files.

---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import Signup from './pages/Signup.jsx'
+import Signin from './pages/Signin.jsx'
+import Profile from './pages/Profile.jsx'
+import Settings from './pages/Settings.jsx'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes> 
+          <Route path='/' element={<Home />} />
+          <Route path='/signup' element={<Signup />} />
+          <Route path='/signin' element={<Signin />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/profile/:username' element={<Profile />} />
+          <Route path='/settings' element={<Settings />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
